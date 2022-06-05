f720c0ca6969527ef5cd9446b9ab93dfabd68b99
thierno <tbbarry8@gmail.com>
2022-06-05 19:11:41 +0200

    add dependency in pom file

--- src/main/java/petition/petition.java
+++ src/main/java/petition/petition.java
@@ -1,9 +1,10 @@
+package main.java.petition;
 
-public class Petition {
+public class petition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-           System.out.print("Bonjour site de petition");
+
 	}
 
 }
